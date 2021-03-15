200feb338b95ba3ff060ccaf07391686d136cb7c
Pedro Baptista <ze.pedro3622@outlook.pt>
2021-03-15 19:47:37 +0000

    Plot start; Variable names update

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 import sys
+import matplotlib.pyplot as plt
 
 
 class Coord:
@@ -9,40 +10,45 @@ class Coord:
         self.best_unit = best_unit
 
 
-def parse(file_name):
+def parse(file_name, plot_min_rx, plot_max_rx, plot_number_bins):
     f = open("CoverageResults/" + file_name, "r")
+
+    # ========== Parsing Section
 
     # Range Read
     range_line = f.readline()
     range_line_info = range_line.replace(",", ".").split("	")
     max_rx = float(range_line_info[1][:-2])
-    # range / 10 + 1 -> for greater than max_rx
-    rx_interval = [0 for i in range(int(round(max_rx / 10)))]
+
+    # rx_interval ==> [0, max_rx/plot_number_bins[ , (...) , [max_rx, 0[
+    rx_interval = [0 for i in range(int(round(max_rx / plot_number_bins)))]
 
     # Units Read
     units = []  # list of tuples: (id, name)
-    best_units = [0]
+
+    # frequency_best_units[2] ==> number of times unit 2 appears as the best unit in the file
+    frequency_best_units = [0]
 
     unit_line = f.readline()
     unit_line_split = unit_line.split("	")
 
-    # while line != "Latitude	Longitude	Rx(dB)	Best unit"
+    # while line != "Latitude	Longitude	Rx(dB)	Best unit" AKA Start of rx values
     while len(unit_line_split) != 4:
         # Adds a tuple with unit id and name to the units list
         units.append((unit_line_split[1], unit_line_split[2]))
-        best_units.append(0)
+        frequency_best_units.append(0)
 
         unit_line = f.readline()
         unit_line_split = unit_line.split("	")
 
-    # Number rx above max
+    # Number rx above max_rx
     rx_above_max = 0
 
     # Number rx below zero
     rx_below_zero = 0
 
     # Contains all rx values
-    rx_values = []
+    all_rx_values = []
 
     # Until the end of the file
     while True:
@@ -54,27 +60,27 @@ def parse(file_name):
         if current_line == "":
             break
 
-        # Best_units update
-        # Increments best units list
+        # Increments frequency_best_units list
         best_unit_index = int(cl_s[3])
-        best_units[best_unit_index] = best_units[best_unit_index] + 1
+        frequency_best_units[best_unit_index] = frequency_best_units[best_unit_index] + 1
 
         # Rx_interval update
-        rx_int = float(cl_s[2].replace(",", "."))
-        rx_values.append(rx_int)
+        rx_float = float(cl_s[2].replace(",", "."))
+        all_rx_values.append(rx_float)
 
-        if rx_int >= max_rx:
+        if rx_float >= max_rx:
             # Values above max are separated
             rx_above_max = rx_above_max + 1
-        elif rx_int < 0:
+        elif rx_float < 0:
             # Negative values are separated
             rx_below_zero = rx_below_zero + 1
         else:
             # Values in bounds
-            index = int(rx_int // 10)
+            index = int(rx_float // 10)
             rx_interval[index] = rx_interval[index] + 1
 
-    # Prints results
+    # ========== Result Printing Section
+
     print("Max Rx:" + str(max_rx))
 
     print("Number rx below 0: " + str(rx_below_zero))
@@ -84,13 +90,36 @@ def parse(file_name):
 
     print("UNITS")
     print(units)
-    print("Best Units: ")
-    print(best_units)
+    print("Frequency Best Units: ")
+    print(frequency_best_units)
+
+    # ========== Plot Section
+
+    # setting the ranges and no. of intervals
+    range_x = (plot_min_rx, plot_max_rx)
+    bins = plot_number_bins
+
+    # plotting a histogram
+    plt.hist(all_rx_values, bins, range_x, color='green',
+             histtype='bar', rwidth=0.8)
+
+    # x-axis label
+    plt.xlabel('Rx(dB)')
+    # frequency label
+    plt.ylabel('Number of Entries')
+    # plot title
+    plt.title('Frequency of Rx Values')
+
+    # function to show the plot
+    plt.show()
 
 
 if __name__ == "__main__":
+    print(sys.argv)
     # Invalid number of arguments
-    if len(sys.argv) > 2 or len(sys.argv) == 1:
-        print("Usage: " + sys.argv[0] + " <file name>")
+    if len(sys.argv) != 5:
+        print(
+            "Usage: " + sys.argv[0] + " <file name> <plot min rx> <plot max rx> <plot number bins>")
     else:
-        parse(sys.argv[1])
+        parse(sys.argv[1], float(sys.argv[2]),
+              float(sys.argv[3]), int(sys.argv[4]))
